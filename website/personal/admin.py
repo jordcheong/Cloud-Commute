c5c2762c4152a505726ece85601ddc37431f1c91
jordcheong <jordcheong@gmail.com>
2017-04-24 00:00:30 -0400

    models

--- website/personal/admin.py
+++ website/personal/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import work_location
 
 # Register your models here.
+
+admin.site.register(work_location)
--- website/personal/models.py
+++ website/personal/models.py
@@ -7,8 +7,12 @@ class work_location(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name +' - '+ self.location
+
+
 class route(models.Model):
     work = models.ForeignKey(work_location, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    current_location = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
